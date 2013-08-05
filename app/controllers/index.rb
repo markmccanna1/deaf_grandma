f202e78bb3b66f1d195e8c2981080122994f155b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 16:57:11 -0500

    Finished

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,5 +5,13 @@ get '/' do
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+
+  if params[:user_input] == params[:user_input].upcase
+    # she heard you
+    redirect to("/?grandma=No%20solicitors!")
+  else
+    # she didn't hear you
+    redirect to("/?grandma=Speak%20up,%20son!")
+  end
 end
